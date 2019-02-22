12a41e198e3b51b63a4c2629135a8fce78df05fb
李威 <LiryLi@wzl.com>
2019-02-22 09:46:05 +0800

    1.清理文件

--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
-#include "widget.h"
-#include <QApplication>
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    Widget w;
-    w.show();
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
-    return a.exec();
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+    if (engine.rootObjects().isEmpty())
+        return -1;
+
+    return app.exec();
 }
